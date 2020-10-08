9474b951d0c55b60443a7c27db842d6e41d06337
zzzxxx00019 <zzzxxx00019@gmail.com>
2020-10-08 11:40:55 +0800

    Modify code with __builtin_ctz and ruduce array

--- totalHammingDistance.c
+++ totalHammingDistance.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int totalHammingDistance(int* nums, int numsSize)
 {
     int distance = 0 ;
@@ -14,6 +14,25 @@ int totalHammingDistance(int* nums, int numsSize)
 
     return distance ;
 }
+*/
+int totalHammingDistance(int *nums, int numsSize)
+{
+    if (!nums || !numsSize) return 0 ;
+    
+    int distance = 0 ;
+    int bit1[32] = {0} ;
+    
+    for (int i = 0 ; i < numsSize ; i++){
+        while(nums[i]){
+            bit1[ __builtin_ctz(nums[i]) ] ++ ;
+            nums[i] ^= nums[i] & -nums[i] ;
+        }
+    }
+    for (int i = 0 ; i < 32 ; i++ ){
+        distance += bit1[i] * (numsSize - bit1[i]) ;
+    }
+    return distance ;
+}
 
 int main()
 {
@@ -21,6 +40,7 @@ int main()
     int nums[] = {4, 14, 2} ;
 
     printf("%d\n",totalHammingDistance(nums, 3)) ;
+    //printf("%d\n",__builtin_ctz(2)) ;
 
     return 0 ;
 }
